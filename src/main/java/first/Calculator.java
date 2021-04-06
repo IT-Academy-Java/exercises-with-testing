86eaad3982c922488b9a94898ddfc73420b2c2ca
Elena Piaggio <elenapiaggio@gmail.com>
2021-04-06 23:27:38 +0200

    ADD substract test

--- src/main/java/first/Calculator.java
+++ src/main/java/first/Calculator.java
@@ -13,11 +13,31 @@ public class Calculator {
         this.secondNumber = secondNumber;
     }
 
+    public int getFirsNumber() {
+        return firsNumber;
+    }
+
+    public void setFirsNumber(int firsNumber) {
+        this.firsNumber = firsNumber;
+    }
+
+    public int getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(int secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
     public int add(Integer firsNumber, Integer secondNumber){
         int result = 0;
         if(firsNumber instanceof Integer  && secondNumber instanceof Integer){
-            result =  firsNumber + secondNumber;
+            result = firsNumber + secondNumber;
         }
         return result;
+    }
+
+    public int subtract(){
+        return this.firsNumber - this.secondNumber;
     }
 }
--- src/test/java/first/CalculatorShould.java
+++ src/test/java/first/CalculatorShould.java
@@ -1,6 +1,5 @@
 package first;
 
-
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -19,6 +18,13 @@ public class CalculatorShould {
     @Test
     public void addTwoNumbers(){
         assertThat(calculator.add(2,5), is(7));
+    }
+
+    @Test
+    public void subtractTwoNumbers(){
+        calculator.setFirsNumber(10);
+        calculator.setSecondNumber(5);
+        assertThat(calculator.subtract(), is(5));
     }
 
 }
